d6c3171337a9069ad04146da52cf0e3fda802eda
EmersonSinezio <emerson.sineziio@gmail.com>
2025-04-28 09:29:01 -0300

    Fix: database on

--- backend/src/config/db.Config.js
+++ backend/src/config/db.Config.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 mongoose.Promise = global.Promise;
 
 // Conecta ao MongoDB
 const connectDB = () => {
-  return mongoose.connect(
-    "mongodb+srv://emersonsineziio:drRYnadsNsnXHkK4@cluster0.okd2t.mongodb.net/dayNotes?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Aumente o timeout para 30 segundos
-    }
-  );
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  });
 };
 
 module.exports = {
