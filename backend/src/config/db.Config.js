4d42998ed989d6e87a2e09f876329d0de7ed1b01
EmersonSinezio <emerson.sineziio@gmail.com>
2025-05-03 20:09:20 -0300

    fix db

--- backend/src/config/db.Config.js
+++ backend/src/config/db.Config.js
@@ -1,19 +1,44 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
-mongoose.Promise = global.Promise;
+console.log(process.env.MONGODB_URI);
 
-// Conecta ao MongoDB
-const connectDB = () => {
-  return mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-  });
+if (!process.env.MONGODB_URI) {
+  throw new Error("🔴 MONGODB_URI não definida no .env");
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const mongooseOptions = {
+  serverSelectionTimeoutMS: 15000,
+  socketTimeoutMS: 30000,
+  ssl: true,
+  tlsAllowInvalidCertificates: false,
 };
 
-module.exports = {
-  mongoose,
-  connectDB,
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  try {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, mongooseOptions)
+      .then((mongoose) => {
+        console.log(`✅ Conectado em: ${mongoose.connection.host}`);
+        return mongoose;
+      });
+
+    cached.conn = await cached.promise;
+  } catch (error) {
+    console.error("❌ Erro de conexão:", error);
+    cached.promise = null;
+    throw new Error("Falha na conexão com o MongoDB");
+  }
+
+  return cached.conn;
 };
+
+export { mongoose, connectDB };
