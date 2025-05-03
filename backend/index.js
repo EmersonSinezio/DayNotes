0ac4ebda29f401ceb87882f91f896daeda49504e
EmersonSinezio <emerson.sineziio@gmail.com>
2025-05-03 19:53:27 -0300

    fix

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./src/config/db.Config");
+const routes = require("./src/routes");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  try {
+    console.log("📡 Iniciando conexão...");
+    await connectDB();
+    next();
+  } catch (err) {
+    console.error("💥 Erro durante a conexão:");
+    console.error(err);
+    res.status(500).json({
+      error: "Erro de conexão com o banco de dados",
+      details:
+        process.env.NODE_ENV === "development" && err instanceof Error
+          ? err.message
+          : undefined,
+    });
+  }
+});
+// Rotas
+app.use(routes);
+
+// Exportação para Vercel
+module.exports = app;
